b0560c71a330a8dcef6f21a80242e0e52b9e509c
manojtjacob <51782418+manojtjacob@users.noreply.github.com>
2021-02-01 19:36:19 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -2,3 +2,9 @@ output "redis_cache_id" {
   description = "Redis Cache Id"
   value       = azurerm_redis_cache.this.id
 }
+
+module "example" {
+  source = "./modules/example"
+
+  ami = aws_ami_copy.example
+}
